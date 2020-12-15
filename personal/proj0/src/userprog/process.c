fcc41f3a2429a93e42e3b5474d2d674437646a2d
yucheng <yuchengscu@gmail.com>
2020-12-15 12:58:20 +0000

    complete prog0

--- personal/proj0/src/userprog/process.c
+++ personal/proj0/src/userprog/process.c
@@ -442,7 +442,7 @@ setup_stack (void **esp)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
-        *esp = PHYS_BASE;
+        *esp = PHYS_BASE-100;   // Remain 100 bytes space for user stack 
       else
         palloc_free_page (kpage);
     }
